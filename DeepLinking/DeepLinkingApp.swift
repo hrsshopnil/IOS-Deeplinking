3fdd5cf3f3bc995d5911f3b04f982f14452f7370
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-10-02 00:18:30 +0600

    Initial Commit

---
+++ DeepLinking/DeepLinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkingApp.swift
+//  DeepLinking
+//
+//  Created by shopnil hasan on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
